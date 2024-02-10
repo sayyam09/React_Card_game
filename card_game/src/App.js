148f3f72bb31405be2de80c8efa40ae35c93f476
sayyam09 <128085244+sayyam09@users.noreply.github.com>
2024-02-10 15:16:32 +0530

    Updated for temporary purpose App.js

--- card_game/src/App.js
+++ card_game/src/App.js
@@ -4,14 +4,14 @@ import Card from './Card';
 
 function App() {
   const images = [
-    'mango.jpg',
-    'orange.jpg',
     'grapes.jpg',
-    'apple.jpg',
-    'banana.jpg',
-    'watermelon.jpg',
-    'fig.jpg',
-    'dragon_fruit.jpg',
+    'grape.jpg',
+    'grapes.jpg',
+    'grape.jpg',
+    'grape.jpg',
+    'grape.jpg',
+    'grape.jpg',
+    'grape.jpg',
   ];
 
   const shuffledImages = [...images, ...images].sort(() => Math.random() - 0.5);
